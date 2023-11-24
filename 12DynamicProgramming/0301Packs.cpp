0048d3c3209bfe3b9835e302de33202d79e0f968
Lrmmm <1136942787@qq.com>
2023-11-25 01:29:43 +0800

    I

--- 12DynamicProgramming/0301Packs.cpp
+++ 12DynamicProgramming/0301Packs.cpp
@@ -81,14 +81,12 @@ namespace knapsack3{
 int KnapSack(const vector<int>& wt, const vector<int>& val,int capacity){
 
     int n = wt.size();
-    vector<vector<int>> dp(n+1,vector<int>(capacity+1));
-    
-    for (int i = 0; i <= n; i++)    
-        for (int j = 0; j <= capacity; j++)
+    vector<vector<int>> dp(n+1,vector<int>(capacity+1,0));
+
+    for (int i = 1; i <= n; i++)    
+        for (int j = 1; j <= capacity; j++)
         {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if(wt[i -1] <= j)
+            if(wt[i -1] <= j)
                 dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]] , dp[i-1][j]);
             else 
                 dp[i][j] = dp[i-1][j];
